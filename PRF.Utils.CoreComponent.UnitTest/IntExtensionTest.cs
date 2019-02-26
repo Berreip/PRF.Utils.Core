91931c4e6c9c563465c06f6299a3327d6d04af6f
pierre bony <pierre.bony@hotmail.fr>
2019-02-26 20:25:55 +0100

    Ajout projet TU et config R#

---
+++ PRF.Utils.CoreComponent.UnitTest/IntExtensionTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PRF.Utils.CoreComponents.Extensions;
+
+namespace PRF.Utils.CoreComponent.UnitTest
+{
+    [TestClass]
+    public class IntExtensionTest
+    {
+        [TestMethod]
+        public void IsInRangeV1()
+        {
+            //Configuration
+            const int nb = 45;
+
+            //Test
+            var res = nb.IsInRange(40, 50);
+
+            //Verify
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod]
+        public void IsInRangeV2()
+        {
+            //Configuration
+            const int nb = 35;
+
+            //Test
+            var res = nb.IsInRange(40, 50);
+
+            //Verify
+            Assert.IsFalse(res);
+        }
+
+    }
+}
