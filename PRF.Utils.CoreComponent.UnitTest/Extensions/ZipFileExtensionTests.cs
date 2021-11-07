931eb4ee3117d67de216d04698a334319afe0b33
berreip <pierre.bony@hotmail.fr>
2021-11-07 15:43:13 +0100

    test unitaires

---
+++ PRF.Utils.CoreComponent.UnitTest/Extensions/ZipFileExtensionTests.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.IO.Compression;
+using NUnit.Framework;
+using PRF.Utils.CoreComponents.Extensions;
+
+namespace PRF.Utils.CoreComponent.UnitTest.Extensions
+{
+    [TestFixture]
+    internal sealed class ZipFileExtensionTests
+    {
+        [Test]
+        public void CreateFileEntryFromString()
+        {
+            //Arrange
+            using (var memoryStream = new MemoryStream())
+            using (var archive = new ZipArchive(memoryStream, ZipArchiveMode.Create, true))
+            {
+                //Act
+                archive.CreateFileEntryFromString("foo.txt", "content");
+            }
+
+            //Assert
+
+        }
+
+    }
+}
--- PRF.Utils.CoreComponents/Extensions/ListFilteringExtension.cs
+++ PRF.Utils.CoreComponents/Extensions/ListFilteringExtension.cs
@@ -4,32 +4,21 @@ using System.Collections.Generic;
 namespace PRF.Utils.CoreComponents.Extensions
 {
     /// <summary>
-    /// Module d'extension de filtre de listes
+    /// Extensions methods for list filtering
     /// </summary>
     public static class ListFilteringExtension
     {
         /// <summary>
-        /// A partir d'une liste de N éléments et un nombre d'éléments cible C où N >= C
-        /// on filtre jusqu'à obtenir le nombre d'éléments voulus dans la liste en tirant au hasard les élément à supprimer
-        ///  -> ATTENTION: la liste elle même est filtrée, on n'en renvoie pas une copie
+        /// Filter a list up to the given number of element by removing at raandom some element. WARNING: the list itself is filtered, not a copy
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="listToFilter">la liste à filtrer</param>
-        /// <param name="maxItemTarget">le nombre d'élément cible</param>
         public static void CapRandomized<T>(this List<T> listToFilter, int maxItemTarget)
         {
             listToFilter.CapRandomized(maxItemTarget, new Random());
         }
 
         /// <summary>
-        /// A partir d'une liste de N éléments et un nombre d'éléments cible C où N >= C
-        /// on filtre jusqu'à obtenir le nombre d'éléments voulus dans la liste en tirant au hasard les élément à supprimer
-        ///  -> ATTENTION: la liste elle même est filtrée, on n'en renvoie pas une copie
+        /// Filter a list up to the given number of element by removing at raandom some element. WARNING: the list itself is filtered, not a copy
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="listToFilter">la liste à filtrer</param>
-        /// <param name="maxItemTarget">le nombre d'élément cible</param>
-        /// <param name="random">le seed pour la randomisation (optionnel)</param>
         public static void CapRandomized<T>(this List<T> listToFilter, int maxItemTarget, Random random)
         {
             if (maxItemTarget > listToFilter.Count)
@@ -41,7 +30,6 @@ namespace PRF.Utils.CoreComponents.Extensions
                 return;
             }
 
-            // tant que la liste est plus grande que la cible, on tire au sort un élément à retirer
             while (listToFilter.Count > maxItemTarget)
             {
                 listToFilter.RemoveAt(random.Next(0, listToFilter.Count - 1));
@@ -49,15 +37,13 @@ namespace PRF.Utils.CoreComponents.Extensions
         }
 
         /// <summary>
+        /// return a random element from the list
         /// Renvoie un élément tiré au hasard de la liste
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="list">la liste à filtrer</param>
-        /// <param name="random">le seed pour la randomisation (optionnel)</param>
-        public static T GetRandomElement<T>(this List<T> list, Random random)
+        public static T GetRandomElement<T>(this IReadOnlyList<T> list, Random random)
         {
             return list.Count == 0
-                ? default(T)
+                ? default
                 : list[random.Next(0, list.Count - 1)];
         }
 
