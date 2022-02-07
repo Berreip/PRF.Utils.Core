2f48fa62b57e936180dabafa6ef562b224e615fd
berreip <pierre.bony@hotmail.fr>
2022-02-07 22:42:30 +0100

    Ajout d'un async locker

---
+++ PRF.Utils.CoreComponent.UnitTest/Async/AsyncLockerTests.cs
@@ -0,0 +1,108 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PRF.Utils.CoreComponents.Async;
+// ReSharper disable UnusedVariable
+
+namespace PRF.Utils.CoreComponent.UnitTest.Async
+{
+    [TestFixture]
+    internal sealed class AsyncLockerTests
+    {
+        [Test]
+        [Timeout(500)]
+        public void AsyncLocker_WaitLock_no_deadlock_case()
+        {
+            //Arrange
+            var locker = new AsyncLocker();
+            var i = 0;
+
+            //Act
+            using (var key = locker.WaitLock())
+            {
+                i++;
+            }
+
+            //Assert
+            Assert.AreEqual(1, i);
+        }
+        
+        [Test]
+        [Timeout(500)]
+        public async Task AsyncLocker_WaitLockAsync_no_deadlock_case()
+        {
+            //Arrange
+            var locker = new AsyncLocker();
+            var i = 0;
+
+            //Act
+            using (var key = await locker.WaitLockAsync())
+            {
+                i++;
+            }
+
+            //Assert
+            Assert.AreEqual(1, i);
+        }
+
+        [Test]
+        [Timeout(500)]
+        public void AsyncLocker_WaitLock_nominal_usage()
+        {
+            //Arrange
+            var locker = new AsyncLocker();
+            var i = 0;
+
+            //Act
+            Parallel.For(0, 1_000, _ =>
+            {
+                using (var key = locker.WaitLock())
+                {
+                    i++;
+                }
+            });
+
+            //Assert
+            Assert.AreEqual(1_000, i);
+        }
+        
+        [Test]
+        [Timeout(500)]
+        public async Task AsyncLocker_WaitLockAsync_nominal_usage()
+        {
+            //Arrange
+            var locker = new AsyncLocker();
+            var i = 0;
+            var tasks = Enumerable.Range(0, 1_000).Select(o => Task.Run(async () =>
+            {
+                using (var key = await locker.WaitLockAsync().ConfigureAwait(false))
+                {
+                    i++;
+                }
+            })).ToArray();
+
+            //Act
+            await Task.WhenAll(tasks);
+
+            //Assert
+            Assert.AreEqual(1_000, i);
+        }
+        
+        [Test]
+        [Timeout(500)]
+        public void AsyncLocker_Double_dispose_check()
+        {
+            //Arrange
+            var locker = new AsyncLocker();
+
+            //Act
+            using (var key = locker.WaitLock())
+            {
+                key.Dispose();
+            }
+
+            //Assert
+            Assert.Pass();
+        }
+    }
+}
---
+++ PRF.Utils.CoreComponents/Async/AsyncLocker.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PRF.Utils.CoreComponents.Async
+{
+    /// <summary>
+    /// a locker that allow async lock (be very carrefull with this kind of lock as a deadlock is very common)
+    /// </summary>
+    public interface IAsyncLocker
+    {
+        /// <summary>
+        /// wait a new locker by blocking the current thread IT HAS TO BE USED IN A USING STATEMENT AS IT IS THE DISPOSE THAT RELEASE THE LOCK
+        /// </summary>
+        IAsyncLock WaitLock();
+        
+        /// <summary>
+        /// wait a new locker in async (do not block current thread) IT HAS TO BE USED IN A USING STATEMENT AS IT IS THE DISPOSE THAT RELEASE THE LOCK
+        /// </summary>
+        Task<IAsyncLock> WaitLockAsync();
+    }
+
+    /// <inheritdoc />
+    public sealed class AsyncLocker : IAsyncLocker
+    {
+        private readonly SemaphoreSlim _semaphoreSlim;
+
+        /// <summary>
+        /// Create a new AsyncLocker (prefer keeping it a a readonly field) that allow requesting for new lock 
+        /// </summary>
+        public AsyncLocker()
+        {
+            _semaphoreSlim = new SemaphoreSlim(1);
+        }
+
+        /// <inheritdoc />
+        public IAsyncLock WaitLock()
+        {
+            return new AsyncLock(_semaphoreSlim).Wait();
+        }
+
+        /// <inheritdoc />
+        public async Task<IAsyncLock> WaitLockAsync()
+        {
+            return await new AsyncLock(_semaphoreSlim).WaitAsync().ConfigureAwait(false);
+        }
+
+        private sealed class AsyncLock : IAsyncLock
+        {
+            private readonly SemaphoreSlim _semaphoreSlim;
+            private bool _disposed;
+
+            public AsyncLock(SemaphoreSlim semaphoreSlim)
+            {
+                _semaphoreSlim = semaphoreSlim;
+            }
+
+
+            public IAsyncLock Wait()
+            {
+                _semaphoreSlim.Wait();
+                return this;
+            }
+            
+            public void Dispose()
+            {
+                if (_disposed)
+                {
+                    return;
+                }
+                _semaphoreSlim.Release();
+                _disposed = true;
+            }
+
+            public async Task<IAsyncLock> WaitAsync()
+            {
+                await _semaphoreSlim.WaitAsync().ConfigureAwait(false);
+                return this;
+            }
+        }
+    }
+
+    /// <summary>
+    /// A lock object to use in a USING that will act as a lock key
+    /// </summary>
+    public interface IAsyncLock : IDisposable
+    {
+    }
+}
