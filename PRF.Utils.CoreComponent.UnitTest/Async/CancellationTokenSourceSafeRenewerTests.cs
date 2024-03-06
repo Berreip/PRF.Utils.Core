17f52e5c4a384b931cfb64ff6eaca63381379632
berreip <pierre.bony@hotmail.fr>
2024-03-06 12:06:22 +0100

    Add a Cancel_ShouldCancelPreviousToken

---
+++ PRF.Utils.CoreComponent.UnitTest/Async/CancellationTokenSourceSafeRenewerTests.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PRF.Utils.CoreComponents.Async;
+
+namespace PRF.Utils.CoreComponent.UnitTest.Async;
+
+internal sealed class CancellationTokenSourceRenewerTests
+{
+    private CancellationTokenSourceSafeRenewer _sut;
+
+    [SetUp]
+    public void TestInitialize()
+    {
+        _sut = new CancellationTokenSourceSafeRenewer();
+    }
+
+
+    [Test]
+    public void ctor_nominalUsage()
+    {
+        //Arrange
+
+        //Act
+        var res = _sut.GetNewTokenAndCancelPrevious();
+
+        //Assert
+        Assert.IsNotNull(res);
+    }
+
+    [Test]
+    public void ctor_nominalUsage_call_twice_do_not_returns_same_token()
+    {
+        //Arrange
+
+        //Act
+        var token1 = _sut.GetNewTokenAndCancelPrevious();
+        var token2 = _sut.GetNewTokenAndCancelPrevious();
+
+        //Assert
+        Assert.IsTrue(token1.IsCancellationRequested);
+        Assert.IsFalse(token2.IsCancellationRequested);
+    }
+
+    [Test]
+    public void GetNewTokenAndCancelPrevious_ShouldCreateNewTokenAndCancelPrevious()
+    {
+        // Arrange
+        var previousToken = _sut.GetToken();
+
+        // Act
+        var newToken = _sut.GetNewTokenAndCancelPrevious();
+
+        // Assert
+        Assert.AreNotSame(previousToken, newToken);
+        Assert.IsTrue(previousToken.IsCancellationRequested);
+        Assert.IsFalse(newToken.IsCancellationRequested);
+    }
+
+    [Test]
+    public void GetToken_ShouldReturnLinkedToken()
+    {
+        // Arrange
+        var previousToken = _sut.GetToken();
+
+        // Act
+        var token = _sut.GetToken();
+
+        // Assert
+        Assert.IsFalse(previousToken.IsCancellationRequested);
+        Assert.IsFalse(token.IsCancellationRequested);
+    }
+
+    [Test]
+    public void GetToken_ShouldReturnLinkedToken_both_cancelled_after_Cancel()
+    {
+        // Arrange
+        var previousToken = _sut.GetToken();
+        var token = _sut.GetToken();
+
+        // Act
+        _sut.Cancel();
+
+        // Assert
+        Assert.IsTrue(previousToken.IsCancellationRequested);
+        Assert.IsTrue(token.IsCancellationRequested);
+    }
+
+    [Test]
+    public void GetToken_WhenNoPreviousToken_ShouldCreateNewToken()
+    {
+        // Act
+        var token = _sut.GetToken();
+
+        // Assert
+        Assert.IsNotNull(token);
+    }
+
+    [Test]
+    public void Cancel_ShouldCancelPreviousToken()
+    {
+        // Arrange
+        var previousToken = _sut.GetToken();
+
+        // Act
+        _sut.Cancel();
+
+        // Assert
+        Assert.IsTrue(previousToken.IsCancellationRequested);
+    }
+
+    [Test]
+    public void Cancel_WhenNoPreviousToken_ShouldNotThrowException()
+    {
+        // Act & Assert
+        Assert.DoesNotThrow(() => _sut.Cancel());
+    }
+
+    [Test]
+    public void GetToken_WhenCancelled_ShouldThrowOperationCanceledException()
+    {
+        // Arrange
+        var token = _sut.GetToken();
+        _sut.Cancel();
+
+        // Act & Assert
+        Assert.ThrowsAsync<TaskCanceledException>(async () => await Task.Delay(100, token).ConfigureAwait(false));
+    }
+}
---
+++ PRF.Utils.CoreComponents/Async/CancellationTokenSourceSafeRenewer.cs
@@ -0,0 +1,59 @@
+﻿using System.Threading;
+
+namespace PRF.Utils.CoreComponents.Async
+{
+    /// <summary>
+    /// Provide an access to a cancellation token and allow to renew the source at any given time in a thread safe way
+    /// </summary>
+    public sealed class CancellationTokenSourceSafeRenewer
+    {
+        private readonly object _key = new object();
+        private CancellationTokenSource _cts;
+
+        /// <summary>
+        /// Return a new CancellationToken and cancel any previous source
+        /// </summary>
+        public CancellationToken GetNewTokenAndCancelPrevious()
+        {
+            lock (_key)
+            {
+                // cancel previous if any
+                _cts?.Cancel();
+                // and renew the source
+                _cts = new CancellationTokenSource();
+                return _cts.Token;
+            }
+        }
+
+        /// <summary>
+        /// Return a token linked to the previous source
+        /// </summary>
+        public CancellationToken GetToken()
+        {
+            lock (_key)
+            {
+                // create new source if needed
+                if (_cts == null)
+                {
+                    _cts = new CancellationTokenSource();
+                }
+                return _cts.Token;
+            }
+        }
+
+        /// <summary>
+        /// Return a token linked to the previous source
+        /// </summary>
+        public void Cancel()
+        {
+            lock (_key)
+            {
+                if (_cts != null)
+                {
+                    _cts.Cancel();
+                    _cts = null;
+                }
+            }
+        }
+    }
+}
