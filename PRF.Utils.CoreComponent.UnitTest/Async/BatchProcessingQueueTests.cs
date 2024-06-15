9688f84c59bde2e56b079958a76ebe264109f0ab
berreip <pierre.bony@hotmail.fr>
2024-06-15 20:51:04 +0200

    Add a basic BatchProcessingQueue

---
+++ PRF.Utils.CoreComponent.UnitTest/Async/BatchProcessingQueueTests.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PRF.Utils.CoreComponents.Async;
+
+namespace PRF.Utils.CoreComponent.UnitTest.Async;
+
+[TestFixture]
+public sealed class BatchProcessingQueueTests
+{
+    private ConcurrentQueue<int?[]> _pageList;
+
+    [SetUp]
+    public void TestInitialize()
+    {
+        // mock:
+        _pageList = new ConcurrentQueue<int?[]>();
+
+        // software under test:
+    }
+
+    [Test]
+    public void Add_Item_Flushes_When_PageSize_Reached()
+    {
+        // Arrange
+        var mrev = new ManualResetEventSlim();
+        using var sut = new BatchProcessingQueue<int?>(
+            pageMaximumSize: 100,
+            TimeSpan.FromMinutes(1),
+            page =>
+            {
+                _pageList.Enqueue(page);
+                mrev.Set();
+            });
+
+        // Act
+        for (var i = 0; i < 100; i++)
+        {
+            sut.Add(i);
+        }
+
+        var pageRetrieved = mrev.Wait(TimeSpan.FromSeconds(5));
+
+        // Assert
+        Assert.IsTrue(pageRetrieved);
+        Assert.IsTrue(_pageList.TryDequeue(out var singlePage));
+        Assert.IsFalse(_pageList.TryDequeue(out _));
+        Assert.AreEqual(100, singlePage.Length);
+        Assert.AreEqual(100, singlePage.Count(o => o != null));
+    }
+
+    [Test]
+    public void Add_Item_Flushes_When_Multiple_PagesSize_Reached()
+    {
+        // Arrange
+        var mrev = new ManualResetEventSlim();
+        const int pageMaximumSize = 100;
+        const int nbPageToGenerate = 70;
+        using var sut = new BatchProcessingQueue<int?>(
+            pageMaximumSize: pageMaximumSize,
+            TimeSpan.FromMinutes(1),
+            page =>
+            {
+                _pageList.Enqueue(page);
+                if (_pageList.Count == nbPageToGenerate)
+                {
+                    mrev.Set();
+                }
+            });
+
+        // Act
+        const int nbItems = pageMaximumSize*nbPageToGenerate;
+
+        for (var i = 0; i < nbItems; i++)
+        {
+            sut.Add(i);
+        }
+
+        var pageRetrieved = mrev.Wait(TimeSpan.FromSeconds(15));
+
+        // Assert
+        Assert.IsTrue(pageRetrieved);
+        // should be nbPageToGenerate queued
+        for (var i = 0; i < nbPageToGenerate; i++)
+        {
+            Assert.IsTrue(_pageList.TryDequeue(out var singlePage));
+            Assert.AreEqual(100, singlePage.Length);
+            Assert.AreEqual(100, singlePage.Count(o => o != null));
+        }
+        // and no more page after
+        Assert.IsFalse(_pageList.TryDequeue(out _));
+    }
+
+    [Test]
+    public void Add_Item_Does_Not_Flush_Before_PageSize_Reached()
+    {
+        // Arrange
+        var mrev = new ManualResetEventSlim();
+        using var sut = new BatchProcessingQueue<int?>(
+            pageMaximumSize: 100,
+            TimeSpan.FromMinutes(10),
+            page =>
+            {
+                _pageList.Enqueue(page);
+                mrev.Set();
+            });
+
+        // Act
+        sut.Add(1);
+        sut.Add(2);
+        var pageRetrieved = mrev.Wait(TimeSpan.FromMilliseconds(400));
+
+        // Assert
+        Assert.IsFalse(pageRetrieved);
+        Assert.IsFalse(_pageList.TryDequeue(out _));
+    }
+
+    [Test]
+    public void Add_Item_Does_Flush_Before_PageSize_When_timeout_is_Reached()
+    {
+        // Arrange
+        var mrev = new ManualResetEventSlim();
+        using var sut = new BatchProcessingQueue<int?>(
+            pageMaximumSize: 100,
+            TimeSpan.FromMilliseconds(100),
+            page =>
+            {
+                _pageList.Enqueue(page);
+                mrev.Set();
+            });
+
+        // Act
+        sut.Add(1);
+        var pageRetrieved = mrev.Wait(TimeSpan.FromMilliseconds(400));
+
+        // Assert
+        Assert.IsTrue(pageRetrieved);
+        Assert.IsTrue(_pageList.TryDequeue(out var singlePage));
+        Assert.AreEqual(1, singlePage.Length);
+        Assert.AreEqual(1, singlePage.Count(o => o != null));
+        // and no more page after
+        Assert.IsFalse(_pageList.TryDequeue(out _));
+    }
+
+    [Test]
+    public async Task Ensure_that_regular_adding_of_items_do_not_prevent_timer_from_beeing_raised()
+    {
+        // Arrange
+        var mrev = new ManualResetEventSlim();
+        using var sut = new BatchProcessingQueue<int?>(
+            pageMaximumSize: 100_000,
+            TimeSpan.FromMilliseconds(300),
+            page =>
+            {
+                _pageList.Enqueue(page);
+                mrev.Set();
+            });
+        var cts =new CancellationTokenSource();
+
+        // Act
+        sut.Add(1);
+        // ReSharper disable once MethodSupportsCancellation
+        var task = Task.Run(async () =>
+        {
+            try
+            {
+                while (!cts.IsCancellationRequested)
+                {
+                    // ReSharper disable once AccessToDisposedClosure
+                    sut.Add(1);
+                    await Task.Delay(20, cts.Token).ConfigureAwait(false);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // nominal cancellation
+            }
+        });
+
+        var pageRetrieved = mrev.Wait(TimeSpan.FromSeconds(1));
+        cts.Cancel();
+        await task.ConfigureAwait(false);
+
+        // Assert
+        Assert.IsTrue(pageRetrieved);
+        Assert.IsTrue(_pageList.TryDequeue(out _));
+        // no check on content as is may vary widely depending on the delay: we ensure that at leat one raised has been done
+    }
+}
---
+++ PRF.Utils.CoreComponents/Async/BatchProcessingQueue.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Threading;
+
+namespace PRF.Utils.CoreComponents.Async;
+
+/// <summary>
+/// Represent a Queue that allow up to pool items and execute a specific callback when a batch
+/// of items reach the size limit OR if the timeout limit is reached,
+/// depending on the first fulfilled condition.
+/// </summary>
+public sealed class BatchProcessingQueue<T> : IDisposable
+{
+    private readonly int _pageMaximumSize;
+    private readonly TimeSpan _timeout;
+    private readonly Action<T[]> _onFlushCallBack;
+    private T[] _currentPage;
+    private int _currentIndex;
+    private readonly object _key = new object();
+    private readonly Timer _timer;
+    private bool _disposed;
+
+    /// <summary>
+    /// Create a new Batch processing queue
+    /// </summary>
+    /// <param name="pageMaximumSize">the maximum size of the page before flushing automatically. It could be set to Int.MaxValue if no page limit is wanted</param>
+    /// <param name="timeout">the timeout before flushing the page, even if not full</param>
+    /// <param name="onFlushCallBack">The method called to flush the page. WATCH OUT: this method is sync with the timeout OR the last Add so be sure to dispatch it if you do not need it sync</param>
+    public BatchProcessingQueue(
+        int pageMaximumSize,
+        TimeSpan timeout,
+        Action<T[]> onFlushCallBack)
+    {
+        if (pageMaximumSize < 1)
+        {
+            throw new ArgumentException($"{pageMaximumSize} should be greater thant zero");
+        }
+        _pageMaximumSize = pageMaximumSize;
+        _timeout = timeout;
+        _onFlushCallBack = onFlushCallBack;
+        _currentPage = new T[pageMaximumSize];
+        // set a timer but do not start it yet
+        _timer = new Timer(_ => CheckAndProcess(), null, Timeout.InfiniteTimeSpan, timeout);
+    }
+
+    /// <summary>
+    /// Add a new item and dequeue if limit is reached
+    /// </summary>
+    public void Add(T item)
+    {
+        T[] page = null;
+        lock (_key)
+        {
+            if (_currentIndex == 0)
+            {
+                // as soon as we add at least one item, we start the timer:
+                _timer.Change(_timeout, Timeout.InfiniteTimeSpan);
+            }
+            _currentPage[_currentIndex] = item;
+            _currentIndex++;
+            if (_currentIndex == _pageMaximumSize)
+            {
+                page = _currentPage;
+                ResetPage();
+            }
+        }
+
+        if (page != null)
+        {
+            _onFlushCallBack(page);
+        }
+    }
+
+
+    private void CheckAndProcess()
+    {
+        T[] page = null;
+        lock (_key)
+        {
+            if (_currentIndex != 0)
+            {
+                Array.Resize(ref _currentPage, _currentIndex);
+                page = _currentPage;
+                ResetPage();
+            }
+        }
+
+        if (page != null)
+        {
+            _onFlushCallBack(page);
+        }
+    }
+
+    private void ResetPage()
+    {
+        _currentPage = new T[_pageMaximumSize];
+        _currentIndex = 0;
+    }
+
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    private void Dispose(bool disposing)
+    {
+        if (_disposed)
+        {
+            return;
+        }
+
+        if (disposing)
+        {
+            // dispose managed state (managed objects).
+        }
+
+        _timer.Dispose();
+        _disposed = true;
+    }
+
+    /// <summary>
+    /// destructor
+    /// </summary>
+    ~BatchProcessingQueue()
+    {
+        Dispose(false);
+    }
+}
